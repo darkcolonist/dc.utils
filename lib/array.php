373d5a074968cb567febeddb32a61560e06c8076
Christian Noel Reyes <darkcolonist@gmail.com>
2013-06-11 11:14:54 +0800

    Update array.php
    added new function array_to_csv

--- lib/array.php
+++ lib/array.php
@@ -260,3 +260,40 @@ static function to_dropdown_key_val($arr, $null = "- select -"){
 
   return $dd;
 }
+
+/**
+ * Generatting CSV formatted string from an array.
+ * By Sergey Gurevich.
+ */
+static function array_to_scv($array, $header_row = true, $col_sep = ",", $row_sep = "\n", $qut = '"')
+{
+  if (!is_array($array) or !is_array($array[0])) return false;
+
+  $output = "";
+
+  //Header row.
+  if ($header_row)
+  {
+    foreach ($array[0] as $key => $val)
+    {
+      //Escaping quotes.
+      $key = str_replace($qut, "$qut$qut", $key);
+      $output .= "$col_sep$qut$key$qut";
+    }
+    $output = substr($output, 1)."\n";
+  }
+  //Data rows.
+  foreach ($array as $key => $val)
+  {
+    $tmp = '';
+    foreach ($val as $cell_key => $cell_val)
+    {
+      //Escaping quotes.
+      $cell_val = str_replace($qut, "$qut$qut", $cell_val);
+      $tmp .= "$col_sep$qut$cell_val$qut";
+    }
+    $output .= substr($tmp, 1).$row_sep;
+  }
+
+  return $output;
+}
