25661a2fd9c2a81f25383cee7a8abd2fae4244a5
Christian Noel Reyes <darkcolonist@gmail.com>
2013-09-12 10:44:03 +0800

    added new function: truncate_mid

--- lib/string.php
+++ lib/string.php
@@ -45,3 +45,17 @@ static function get_truncated_string($string, $length){
 
   return $val;
 }
+
+/**
+ * truncates a string based on length and replaces middle with truncator
+ */
+static function truncate_mid($string, $length, $truncator = "&hellip;"){
+  $textLength = strlen($string);
+
+  if($textLength <= $length)
+    return $string;
+  
+  $result = substr_replace($string, $truncator, $length/2, $textLength-$length);
+  
+  return $result;
+}
