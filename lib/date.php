321faac916a907d2161b575bc483847baa099c6e
Christian Noel Reyes <darkcolonist@gmail.com>
2014-03-19 09:52:20 +0800

    added ago and agoFB for human readable dates

--- lib/date.php
+++ lib/date.php
@@ -97,3 +97,116 @@ static function timer_current(){
 
   return number_format($total, 2);
 }
+
+/**
+ * Return how long ago this was. eg: 3d 17h 4m 18s ago
+ * @param  string $when date to challenge with gmdate
+ * @return string
+ */
+static function ago($when){
+  if(!strtotime($when))
+    return null;
+
+  // convert when to timestamp
+  $when = date("U", strtotime($when));
+
+  $diff = gmdate("U") - $when;
+
+      // Days
+  $day = floor($diff / 86400);
+  $diff = $diff - ($day * 86400);
+
+      // Hours
+  $hrs = floor($diff / 3600);
+  $diff = $diff - ($hrs * 3600);
+
+      // Mins
+  $min = floor($diff / 60);
+  $diff = $diff - ($min * 60);
+
+      // Secs
+  $sec = $diff;
+
+      // Return how long ago this was. eg: 3d 17h 4m 18s ago
+      // Skips left fields if they aren't necessary, eg. 16h 0m 27s ago / 10m 7s ago
+  $str = sprintf("%s%s%s%s",
+    $day != 0 ? $day."d " : "",
+    ($day != 0 || $hrs != 0) ? $hrs."h " : "",
+    ($day != 0 || $hrs != 0 || $min != 0) ? $min."m " : "",
+    $sec."s ago"
+    );
+
+  return $str;
+}
+
+/**
+ * similar to util::ago except that it follows facebook style
+ * and shows only the relevant ago to be seen: hour only | seconds only
+ * @param  [type] $timestamp [description]
+ * @return [type]            [description]
+ */
+function agoFB($timestamp){
+  //type cast, current time, difference in timestamps
+  $timestamp      = strtotime($timestamp);
+  $current_time   = time();
+  $diff           = $current_time - $timestamp;
+  
+  //intervals in seconds
+  $intervals      = array (
+    'year' => 31556926, 'month' => 2629744, 'week' => 604800, 'day' => 86400, 'hour' => 3600, 'minute'=> 60
+    );
+  
+  //now we just find the difference
+  if ($diff == 0)
+  {
+    return 'now';
+  }    
+
+  if ($diff < 60){
+    return $diff == 1 
+      ? $diff . " second" . ' ago' 
+      : $diff . " seconds" . ' ago';
+  }        
+
+  if ($diff >= 60 && $diff < $intervals['hour']){
+    $diff = floor($diff/$intervals['minute']);
+    return $diff == 1 
+      ? $diff . " minute" . ' ago' 
+      : $diff . " minutes" . ' ago';
+  }        
+
+  if ($diff >= $intervals['hour'] && $diff < $intervals['day']){
+    $diff = floor($diff/$intervals['hour']);
+    return $diff == 1 
+      ? $diff . " hour" . ' ago' 
+      : $diff . " hours" . ' ago';
+  }    
+
+  if ($diff >= $intervals['day'] && $diff < $intervals['week']){
+    $diff = floor($diff/$intervals['day']);
+    return $diff == 1 
+      ? $diff . " day" . ' ago' 
+      : $diff . " days" . ' ago';
+  }    
+
+  if ($diff >= $intervals['week'] && $diff < $intervals['month']){
+    $diff = floor($diff/$intervals['week']);
+    return $diff == 1 
+      ? $diff . " week" . ' ago' 
+      : $diff . " weeks" . ' ago';
+  }    
+
+  if ($diff >= $intervals['month'] && $diff < $intervals['year']){
+    $diff = floor($diff/$intervals['month']);
+    return $diff == 1 
+      ? $diff . " month" . ' ago' 
+      : $diff . " months" . ' ago';
+  }    
+
+  if ($diff >= $intervals['year']){
+    $diff = floor($diff/$intervals['year']);
+    return $diff == 1 
+      ? $diff . " year" . ' ago' 
+      : $diff . " years" . ' ago';
+  }
+}
